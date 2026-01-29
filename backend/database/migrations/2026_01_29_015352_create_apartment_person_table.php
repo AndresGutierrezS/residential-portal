ce41549aefcbeca0551623aba4ff65a45ad52c63
AndresGutierrezS <gerardoandresg2001@gmail.com>
2026-01-28 20:41:40 -0600

    Add core relational migrations (intermediate)

---
+++ backend/database/migrations/2026_01_29_015352_create_apartment_person_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_person', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('person_id')->constrained()->onDelete('cascade');
+            $table->foreignId('apartment_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_resident');
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_person');
+    }
+};
