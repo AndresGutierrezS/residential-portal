19a321b283e31562cddc461e557ce6bee47f3c48
Andres Gutierrez <gerardoandresg2001@gmail.com>
2026-02-06 21:19:38 -0600

    Add initial application routes with role-based layouts

--- frontend/src/app.router.tsx
+++ frontend/src/app.router.tsx
@@ -1,28 +1,78 @@
 import { createBrowserRouter } from "react-router";
+import { ResidentLayout } from "./resident/layouts/ResidentLayout";
+import { AuthLayout } from './auth/layouts/AuthLayout';
+import  { AdminLayout } from "./admin/layouts/AdminLayout";
+import { HomePage } from "./resident/pages/HomePage";
+import { ChatPage } from "./resident/pages/ChatPage";
 
 
 export const appRouter = createBrowserRouter([
       {
         path: '/',
-        element: '#',
+        element: <ResidentLayout />,
         children: [
-
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: 'chat',
+                element: <ChatPage />,
+            }
         ]
       },
 
       {
         path: '/auth',
-        element: '#',
+        element: <AuthLayout />,
         children: [
-
+            {
+                index: true,
+                element: 'login'
+            }
         ]
       },
 
       {
         path: '/admin',
-        element: '#',
+        element: <AdminLayout />,
         children: [
-
+            {
+                index: true,
+                element: 'dashboard'
+            },
+            {
+                path: 'residents',
+                element: 'residents'
+            },
+            {
+                path: 'units',
+                element: 'units'
+            },
+            {
+                path: 'payments',
+                element: 'payments'
+            },
+            {
+                path: 'cars',
+                element: 'cars'
+            },
+            {
+                path: 'expenses',
+                element: 'expenses'
+            },
+            {
+                path: 'events',
+                element: 'events'
+            },
+            {
+                path: 'reports',
+                element: 'reports'
+            },
+            {
+                path: 'chat',
+                element: 'chat'
+            },
         ],
       }
 ]);
