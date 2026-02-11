9ae108bbc1e8e2763a7e6032daf3367c5ef25a67
AndresGutierrezS <gerardoandresg2001@gmail.com>
2026-02-11 11:48:53 -0600

    Add MessageSent broadcast event

--- backend/app/Events/MessageSent.php
+++ backend/app/Events/MessageSent.php
@@ -6,20 +6,28 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct(
+        public string $message,
+        public string $senderId
+    )
     {
         //
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'message.sent';
     }
 
     /**
@@ -30,7 +38,7 @@ class MessageSent
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new Channel('messages'),
         ];
     }
 }
