5e8cd8accfdf76db46ffc36223b5f98860eaf3b1
AndresGutierrezS <gerardoandresg2001@gmail.com>
2026-02-17 16:10:46 -0600

    Add useChat hook

--- frontend/src/chat/actions/sendMessage.action.ts
+++ frontend/src/chat/actions/sendMessage.action.ts
@@ -1,7 +1,7 @@
 import { portalGateApi } from "@/api/portalGateApi";
 import type { Message } from "../interfaces/message.interface";
 
-export const sendMessagesAction = async (message: string): Promise<Message> => {
+export const sendMessageAction = async (message: string): Promise<Message> => {
 
     try {
         const response = await portalGateApi.post<Message>('/chat/messages', {
---
+++ frontend/src/chat/hooks/useChat.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react"
+import type { Message } from "../interfaces/message.interface";
+import { getMessagesAction } from "../actions/getMessages.action";
+import echo from "@/lib/echo";
+import { sendMessageAction } from "../actions/sendMessage.action";
+
+
+export const useChat = async () => {
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        loadMessages();
+        connectSocket();
+
+        return () => {
+            echo.leave('chat');
+        }
+    }, []);
+
+    const loadMessages = async () => {
+        try {
+            const data = await getMessagesAction();
+            setMessages(data); 
+        } catch (error) {
+            throw error;
+        } finally {
+            setIsLoading(false);
+        }
+    }   
+
+    const connectSocket = () => {
+        
+        echo.channel('chat').listen(".message.sent", (event: Message) => {
+            setMessages(prev => [...prev, event]);
+        });
+    }
+
+    const sendMessage = async (message: string) => {
+        try {
+            await sendMessageAction(message);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    return {
+        messages,
+        isLoading,
+        sendMessage,
+    }
+
+}
