d472f5f9becd36dba430e030b15d2f44c6c26ac5
Andres Gutierrez <gerardoandresg2001@gmail.com>
2026-03-30 23:30:07 -0600

    feat(apartments): implement apartment actions

--- frontend/src/admin/apartments/actions/apartments.actions.ts
+++ frontend/src/admin/apartments/actions/apartments.actions.ts
@@ -1,19 +1,37 @@
 import { portalGateApi } from "@/api/portalGateApi";
-import type { ApartmentResponse } from "../interfaces/apartment.interface";
+import type { ApartmentDTO, ApartmentResponse } from "../interfaces/apartment.interface";
 
 
 export const getApartmentsAction = async (): Promise<ApartmentResponse[]> => {
     try {
-        const {data} = await portalGateApi.get<ApartmentResponse[]>('apartments');
+        const { data } = await portalGateApi.get<ApartmentResponse[]>('apartments');
         return data;
     } catch (error) {
         throw error;
     }
 } 
 
-export const createApartmentAction = async () => {
+export const createApartmentAction = async (payload: ApartmentDTO): Promise<ApartmentResponse> => {
     try {
-        const {} = await portalGateApi.post('apartments', )
+        const { data } = await portalGateApi.post<ApartmentResponse>('apartments', payload);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateApartmentAction = async (id: number, payload: ApartmentDTO): Promise<ApartmentResponse> => {
+    try {
+        const { data } = await portalGateApi.put<ApartmentResponse>(`apartments/${id}`, payload);
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteApartmentAction = async (id: number): Promise<void> => {
+    try {
+        await portalGateApi.delete(`apartments/${id}`);
     } catch (error) {
         throw error;
     }
