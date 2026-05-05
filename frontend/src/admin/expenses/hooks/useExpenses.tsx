667392662f628e0400fa8d51141e4474cd6a139a
Andres Gutierrez <gerardoandresg2001@gmail.com>
2026-05-04 19:36:16 -0600

    feat(expenses): add useExpenses hook

---
+++ frontend/src/admin/expenses/hooks/useExpenses.tsx
@@ -0,0 +1,50 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { createExpense, deleteExpense, getExpenses, updateExpense } from "../actions/expenses.actions";
+import type { Expense } from "../interfaces/expense.interface";
+
+export const useExpenses = () => {
+  
+    const queryClient = useQueryClient();
+
+    const expensesQuery = useQuery({
+        queryKey: ['expenses'],
+        queryFn: getExpenses,
+    })
+
+    const createMutation = useMutation({
+        mutationFn: createExpense,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['expenses']});
+        }
+    })
+
+    const updateMutation = useMutation({
+        mutationFn: ({id, data}: {id: number, data: Expense}) => updateExpense(id, data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['expenses']});
+        }
+    })
+
+    const deleteMutation = useMutation({
+        mutationFn: deleteExpense,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['expenses']});
+        }
+    })
+
+    // const expenseQuery = useQuery({
+    //     queryKey: ['expense'],
+    //     queryFn: getExpense,
+    // })
+
+    return {
+        expenses: expensesQuery.data ?? [],
+        
+        isLoading: expensesQuery.isLoading,
+        isError: expensesQuery.isError,
+
+        createExpense: createMutation.mutate,
+        updateExpense: updateMutation.mutate,
+        deleteExpense: deleteMutation.mutate,
+    }
+}
